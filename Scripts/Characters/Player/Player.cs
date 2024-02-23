106fd8321cbacd1b4e8993d53aed16832ac47a77
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-02-23 09:02:00 -0800

    Override physics and input

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        GD.Print("Character Physics logged.");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Input received.");
+    }
 }
