c4b83ea2a23d43c8f51e210d3fe599ad464e9088
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-02-24 11:15:31 -0800

    Set Timer for Dash state

--- Scripts/Characters/Player/Player/DashState.cs
+++ Scripts/Characters/Player/Player/DashState.cs
@@ -4,9 +4,11 @@ using System;
 public partial class DashState : Node
 {
 	private Player characterNode;
+	[Export] private Timer dashTimerNode;
 	public override void _Ready()
 	{
 		characterNode = GetOwner<Player>();
+		dashTimerNode.Timeout += HandleDashTimeout;
 	}
 	public override void _Notification(int what)
 	{
@@ -14,6 +16,11 @@ public partial class DashState : Node
 		if (what == 5001)
 		{
 			characterNode.animPlayerNode.Play(GameConstants.ANIME_DASH);
+			dashTimerNode.Start();
 		}
+	}
+	private void HandleDashTimeout()
+	{
+		characterNode.stateMachineNode.SwitchState<IdleState>();
 	}
 }
