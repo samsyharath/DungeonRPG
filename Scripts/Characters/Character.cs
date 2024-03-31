ed0692f9a60ce2bf21c3a8182fdf6b406618cb85
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-03-31 09:47:07 -0700

    Added hitbox and hurtbox

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -3,10 +3,13 @@ using System;
 
 public abstract partial class Character : CharacterBody3D
 {
+    [Export] private StatResource[] stats;
+
     [ExportGroup("Required Nodes")]
     [Export] public AnimationPlayer AnimPlayerNode {get; private set;}
     [Export] public Sprite3D SpriteNode {get; private set;}
     [Export] public StateMachine StateMachineNode {get; private set;}
+    [Export] public Area3D HurtboxNode {get; private set;}
 
     [ExportGroup("AI Nodes")]
     [Export] public Path3D PathNode {get; private set;}
@@ -17,6 +20,27 @@ public abstract partial class Character : CharacterBody3D
     public Vector2 direction = new();
     public override void _Ready()
     {
+        HurtboxNode.AreaEntered += HandleHurtboxEntered;
+    }
+
+    private void HandleHurtboxEntered(Area3D area)
+    {
+        StatResource health = GetStatResource(Stat.Health);
+        GD.Print(health.StatValue);
+        
+    }
+
+    public StatResource GetStatResource(Stat stat)
+    {
+        StatResource result = null;
+
+        foreach(StatResource element in stats){
+            if (element.StatType == stat)
+            {
+                result = element;
+            }
+        }
+        return result;
     }
 
     public void Flip()
--- Scripts/Characters/Player/Player/AttackState.cs
+++ Scripts/Characters/Player/Player/AttackState.cs
@@ -3,18 +3,27 @@ using System;
 
 public partial class AttackState : PlayerState
 {
+    [Export] private Timer comboTimerNode;
+
     private int comboCounter = 1;
     private int maxComboCount = 2;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        comboTimerNode.Timeout += () => comboCounter = 1;
+    }
     protected override void EnterState()
     {
-        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter, -1, 2.5f);
         characterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
     }
 
     protected override void ExitState()
     {
         characterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+        comboTimerNode.Start();
     }
 
     private void HandleAnimationFinished(StringName animName)
